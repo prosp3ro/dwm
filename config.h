017b7612bb2a91d956d119df0a386cd35aeb934b
prospero <g0lden_vo1d@protonmail.com>
2023-10-08 13:26:59 +0200

    tweaks

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 // static const char *fonts[]          = { "monospace:size=10" };
-static char *fonts[]          = { "monospace:size=11", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"  };
-static const char dmenufont[]       = "monospace:size=12";
+static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"  };
+static const char dmenufont[]       = "monospace:size=11";
 static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#222222";
+static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#123456";
+static char selbordercolor[]        = "#770000";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
@@ -20,7 +20,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "web", "dev", "dev", "dev", "dev", "temp", "temp", "social", "help" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -74,51 +74,63 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 #include "shiftview.c"
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+    /* modifier                     key        function        argument */
+    { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
     { MODKEY|ShiftMask,		XK_d,	spawn,		               SHCMD("keepmenu") },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	STACKKEYS(MODKEY,                          focus)
-	STACKKEYS(MODKEY|ShiftMask,                push)
-	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	// { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	// { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_x,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ShiftMask,		        XK_m,	spawn,		       SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle; kill -10 $(pidof dwmblocks)") },
+    { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_Return, togglescratch,  {.v = scratchpadcmd } },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
+    STACKKEYS(MODKEY,                          focus)
+    STACKKEYS(MODKEY|ShiftMask,                push)
+    // { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+    // { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    // { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY,                       XK_q,      killclient,     {0} },
+    { MODKEY,                       XK_f,      togglefullscr,  {0} },
+    { MODKEY,                       XK_space,  setlayout,      {0} },
+    { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+    // { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+    // { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+    { MODKEY|ShiftMask,		        XK_m,	spawn,		       SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle; kill -10 $(pidof dwmblocks)") },
     { MODKEY,			            XK_minus,	spawn,		   SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%; kill -10 $(pidof dwmblocks)") },
-	{ MODKEY,			            XK_equal,	spawn,		   SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%; kill -10 $(pidof dwmblocks)") },
-	// { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	// { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	// { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	// { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,		                XK_comma,	   shiftview,  { .i = -1 } },
-	{ MODKEY,			            XK_period,	   shiftview,  { .i = 1 } },
-	{ MODKEY,			            XK_BackSpace,	spawn,		   SHCMD("dmenusys") },
-	{ MODKEY|ShiftMask,			            XK_BackSpace,	spawn,		   SHCMD("rebuild dwm") },
-	{ MODKEY,			            XK_grave,	spawn,		   SHCMD("dunstctl close") },
-	{ MODKEY|ShiftMask,			    XK_b,	spawn,		   SHCMD("dmenubluetooth") },
-	{ 0,			                XK_Print,	spawn,		   SHCMD("dmenumaim") },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+    { MODKEY,			            XK_equal,	spawn,		   SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%; kill -10 $(pidof dwmblocks)") },
+    // { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+    // { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+    // { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+    // { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { MODKEY,		                XK_comma,	   shiftview,  { .i = -1 } },
+    { MODKEY,			            XK_period,	   shiftview,  { .i = 1 } },
+    { MODKEY,			            XK_BackSpace,	spawn,		   SHCMD("dmenusys") },
+    { MODKEY|ShiftMask,			            XK_BackSpace,	spawn,		   SHCMD("rebuild dwm") },
+    { MODKEY,			            XK_grave,	spawn,		   SHCMD("dunstctl close") },
+    { MODKEY|ShiftMask,			    XK_b,	spawn,		   SHCMD("dmenubluetooth") },
+    { 0,			                XK_Print,	spawn,		   SHCMD("dmenumaim") },
+    { MODKEY,			                XK_w,	spawn,		   SHCMD("brave-browser-nightly") },
+    { MODKEY,			                XK_c,	spawn,		   SHCMD("chromium") },
+    { MODKEY|ShiftMask,			                XK_f,	spawn,		   SHCMD("ferdium") },
+    { MODKEY|ShiftMask,			                XK_s,	spawn,		   SHCMD("signal-desktop") },
+    { MODKEY,			                XK_n,	spawn,		   SHCMD("obsidian") },
+    { MODKEY|ShiftMask,			                XK_p,	spawn,		   SHCMD("mpc toggle") },
+    { MODKEY|ShiftMask,			                XK_v,	spawn,		   SHCMD("vscodium") },
+    { MODKEY|ShiftMask,			                XK_minus,	spawn,		   SHCMD("mpc volume -5") },
+    { MODKEY|ShiftMask,			                XK_equal,	spawn,		   SHCMD("mpc volume +5") },
+    { MODKEY|ShiftMask,			                XK_bracketleft,	spawn,		   SHCMD("mpc prev") },
+    { MODKEY|ShiftMask,			                XK_bracketright,	spawn,		   SHCMD("mpc next") },
+    { MODKEY,			                XK_F1,	spawn,		   SHCMD("dmenumount") },
+    { MODKEY,			                XK_F2,	spawn,		   SHCMD("dmenuumount") },
+    { MODKEY,			                XK_F3,	spawn,		   SHCMD("dmenudisplay") },
+    TAGKEYS(                        XK_1,                      0)
+    TAGKEYS(                        XK_2,                      1)
+    TAGKEYS(                        XK_3,                      2)
+    TAGKEYS(                        XK_4,                      3)
+    TAGKEYS(                        XK_5,                      4)
+    TAGKEYS(                        XK_6,                      5)
+    TAGKEYS(                        XK_7,                      6)
+    TAGKEYS(                        XK_8,                      7)
+    TAGKEYS(                        XK_9,                      8)
 };
 
 /* button definitions */
@@ -128,7 +140,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	// { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
